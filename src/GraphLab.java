dbb2db525cd46f0d650ff64b0cb6e0b47b7075fb
imadrid181 <imadrid181@gmail.com>
2018-11-29 10:07:34 -0700

    Added User Input

--- src/GraphLab.java
+++ src/GraphLab.java
@@ -3,7 +3,25 @@ public class GraphLab {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("How Vertices do you want the graph to have ?");
+        System.out.println("How many vertices do you want the graph to have ? ");
         int numberOfVertices = input.nextInt();
+
+        System.out.println("How many edges do you want the graph to have ? ");
+        int numberOfEdges = input.nextInt();
+
+        System.out.println("Do you want the graph to be weighted (Y/N) ? ");
+        Char weighted = input.nextChar();
+
+        if(weighted == 'Y') {
+            System.out.println("What do you want the min weight to be ? ");
+            int minWeight = input.nextInt();
+
+            System.out.println("What do you want the max weight to be ? ");
+            int maxWeight = input.nextInt();
+        }
+        
+        System.out.println("How do you want the graph to be represented (List/Matrix) ?");
+        Sting rep = input.nextLine();
+
     }
 }
