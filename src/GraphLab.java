a8fc71ca62d44f662766059b23abcab2841a8c92
imadrid181 <imadrid181@gmail.com>
2018-11-29 09:59:55 -0700

    First Commit

---
+++ src/GraphLab.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class GraphLab {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("How Vertices do you want the graph to have ?");
+        int numberOfVertices = input.nextInt();
+    }
+}
